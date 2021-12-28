9d90d7a45b8402173748871ab9cb616277669846
Dmit93 <81878817+Dmit93@users.noreply.github.com>
2021-12-28 15:05:08 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -591,3 +591,191 @@ document.querySelectorAll('.photo-section__slider .swiper-slide').forEach(i => {
 
 
 // Страница фотогалерея, переключение вкладок //
+
+
+
+let previous = document.querySelector('.sounds__pre');
+let play = document.querySelector('.sounds_section__play');
+let slider = document.querySelector('.sounds_section__duration_slider');
+let next = document.querySelector('.sounds__next');
+//let title = document.querySelector('.sounds__title');
+let recent_volume = document.querySelector('.sounds__volume');
+let volume_show = document.querySelector('.sounds__volume_show');
+
+let show_duration = document.querySelector('.sounds__show_duration');
+let track_image = document.querySelector('.sounds__track_image');
+let auto_play = document.querySelector('.sounds__auto');
+let present = document.querySelector('.sounds__present');
+let total = document.querySelector('.sounds__total');
+let artist = document.querySelector('.sounds__artist');
+
+
+
+let timer;
+let autoplay = 0;
+
+let index_no = 0;
+let Playing_song = false;
+
+//create a audio Element
+let track = document.createElement('audio');
+
+
+//All songs list
+let All_song = [{
+        name: "first song",
+        path: "music/sample-1.mp3",
+        img: "./img/main-slider/1.jpg",
+        singer: "1"
+    },
+    {
+        name: "second song",
+        path: "music/sample-2.mp3",
+        img: "./img/main-slider/1.jpg",
+        singer: "2"
+    },
+    {
+        name: "third song",
+        path: "music/sample-3.mp3",
+        img: "./img/main-slider/1.jpg",
+        singer: "3"
+    },
+
+];
+
+
+// All functions
+
+
+// function load the track
+function load_track(index_no) {
+    clearInterval(timer);
+    reset_slider();
+
+    track.src = All_song[index_no].path;
+    // title.innerHTML = All_song[index_no].name;
+    // track_image.src = All_song[index_no].img;
+    // artist.innerHTML = All_song[index_no].singer;
+    track.load();
+
+    timer = setInterval(range_slider, 1000);
+    // total.innerHTML = All_song.length;
+    //  present.innerHTML = index_no + 1;
+}
+
+load_track(index_no);
+
+
+//mute sound function
+function mute_sound() {
+    track.volume = 0;
+    volume.value = 0;
+    volume_show.innerHTML = 0;
+}
+
+
+// checking.. the song is playing or not
+function justplay() {
+    if (Playing_song == false) {
+        playsong();
+
+    } else {
+        pausesong();
+    }
+}
+
+
+// reset song slider
+function reset_slider() {
+    slider.value = 0;
+}
+
+// play song
+function playsong() {
+    track.play();
+    Playing_song = true;
+    play.innerHTML = '<img src="./img/sounds/pause.png">';
+}
+
+//pause song
+function pausesong() {
+    track.pause();
+    Playing_song = false;
+    play.innerHTML = '<img src="./img/sounds/play.png">';
+}
+
+
+// next song
+function next_song() {
+    if (index_no < All_song.length - 1) {
+        index_no += 1;
+        load_track(index_no);
+        playsong();
+    } else {
+        index_no = 0;
+        load_track(index_no);
+        playsong();
+
+    }
+}
+
+
+// previous song
+function previous_song() {
+    if (index_no > 0) {
+        index_no -= 1;
+        load_track(index_no);
+        playsong();
+
+    } else {
+        index_no = All_song.length;
+        load_track(index_no);
+        playsong();
+    }
+}
+
+
+// change volume
+function volume_change() {
+    volume_show.innerHTML = recent_volume.value;
+    track.volume = recent_volume.value / 100;
+}
+
+// change slider position 
+function change_duration() {
+    slider_position = track.duration * (slider.value / 100);
+    track.currentTime = slider_position;
+}
+
+// autoplay function
+function autoplay_switch() {
+    if (autoplay == 1) {
+        autoplay = 0;
+        auto_play.style.background = "rgba(255,255,255,0.2)";
+    } else {
+        autoplay = 1;
+        auto_play.style.background = "#FF8A65";
+    }
+}
+
+
+function range_slider() {
+    let position = 0;
+
+    // update slider position
+    if (!isNaN(track.duration)) {
+        position = track.currentTime * (100 / track.duration);
+        slider.value = position;
+    }
+
+
+    // function will run when the song is over
+    if (track.ended) {
+        play.innerHTML = '<img src="./img/sounds/play.png">';
+        if (autoplay == 1) {
+            index_no += 1;
+            load_track(index_no);
+            playsong();
+        }
+    }
+}
